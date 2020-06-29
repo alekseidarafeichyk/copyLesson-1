e55953591d434e5592bdb4aded16838f51d1fecc
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-06-29 20:56:15 +0300

    Updates

---
+++ precache-manifest.7b18fb30929e93a052cc4bbdba0ab3aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0464c8f6842d322a8320bf4a288275e1",
+    "url": "/Ignat-Homework/index.html"
+  },
+  {
+    "revision": "f529fbd06d2cd585033a",
+    "url": "/Ignat-Homework/static/css/main.b9a2f74e.chunk.css"
+  },
+  {
+    "revision": "2744f8e5c4f552dec476",
+    "url": "/Ignat-Homework/static/js/2.74648e82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Ignat-Homework/static/js/2.74648e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f529fbd06d2cd585033a",
+    "url": "/Ignat-Homework/static/js/main.32323ea7.chunk.js"
+  },
+  {
+    "revision": "1ea26722f11ad0caf38a",
+    "url": "/Ignat-Homework/static/js/runtime-main.a927f299.js"
+  }
+]);
